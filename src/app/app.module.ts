1458d3a9ad1bb1e71e7cf01fece7d5349ee261ad
Roy <nirmalya.roy@capgemini.com>
2019-09-16 15:48:21 +0530

    Signed-off-by: Roy <nirmalya.roy@capgemini.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { GridComponent } from './ui-components/grid/grid.component';
 import { DropdownsComponent } from './ui-components/dropdowns/dropdowns.component';
 import { AccordionComponent } from './ui-components/accordion/accordion.component';
 import { AccordionTabComponent } from './ui-components/accordion/accordion-tab/accordion-tab.component';
+import { CommonTemplateDirective } from './ui-components/shared/directives/ronx-template.directive';
 @NgModule({
-  declarations: [AppComponent, GridComponent, DropdownsComponent, AccordionComponent, AccordionTabComponent],
+  declarations: [AppComponent,
+     GridComponent, DropdownsComponent,
+      AccordionComponent, AccordionTabComponent, CommonTemplateDirective],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/ui-components/shared/directives/ronx-template.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RonxTemplateDirective } from './ronx-template.directive';
+
+describe('RonxTemplateDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RonxTemplateDirective();
+    expect(directive).toBeTruthy();
+  });
+});
